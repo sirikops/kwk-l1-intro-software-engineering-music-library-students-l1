2cc95916aeb4e3613dc5c6d21f90bc1306156892
sirikops <sirikops@hotmail.com>
2018-06-06 23:01:28 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -17,11 +17,6 @@ def list_artists(hash)
 #2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
 # This method should accept a hash as an argument.
 
-music_library = {
-   "Britney Spears" => "Toxic",
-   "Adele" => "Rolling in the Deep",
-   "The Beatles" => "Yellow Submarine",
-  }
   def list_songs(hash)
     hash.each_value { |song|
     puts song
