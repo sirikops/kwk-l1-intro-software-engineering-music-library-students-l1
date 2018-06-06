c574e3c64399c3f2cc27fddaf0da5f1cbe4d5176
sirikops <sirikops@hotmail.com>
2018-06-06 23:01:21 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -22,7 +22,10 @@ music_library = {
    "Adele" => "Rolling in the Deep",
    "The Beatles" => "Yellow Submarine",
   }
-  def list_songs
+  def list_songs(hash)
+    hash.each_value { |song|
+    puts song
+    }
 
 #3. Define a method `delete_artist`. This method should take delete the key-value pair from the hash based on the artist and returned the updated hash.
 #This method should take two arguments, the music library hash, and the artist. Remember, keys in hashes are symbols, so you'll need to expect the method to accept the artist as a symbol. 
